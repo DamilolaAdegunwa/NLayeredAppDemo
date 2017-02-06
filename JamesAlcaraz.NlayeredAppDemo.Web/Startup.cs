957c98a33519f3ad315dfee293404b004c3be445
alcaraz <alcaraz@DESKTOP-BKM7Q7I>
2017-02-06 21:20:49 +0800

    Add project files.

---
+++ JamesAlcaraz.NlayeredAppDemo.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JamesAlcaraz.NlayeredAppDemo.Web.Startup))]
+namespace JamesAlcaraz.NlayeredAppDemo.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
