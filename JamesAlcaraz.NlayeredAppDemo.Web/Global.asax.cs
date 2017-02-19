274c6671398b95e16883d6b8f2f41576f1a3fe8a
james.alcaraz <alcarazjames@gmail.com>
2017-02-19 09:44:50 +0800

    Setup Web Api on MVC Project

--- JamesAlcaraz.NlayeredAppDemo.Web/Global.asax.cs
+++ JamesAlcaraz.NlayeredAppDemo.Web/Global.asax.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using JamesAlcaraz.NlayeredAppDemo.Web.App_Start;
 
 namespace JamesAlcaraz.NlayeredAppDemo.Web
 {
@@ -14,6 +16,7 @@ namespace JamesAlcaraz.NlayeredAppDemo.Web
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             UnityConfig.RegisterComponents();
