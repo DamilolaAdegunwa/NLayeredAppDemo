b09a0754b3fbaa7b9398f58b53e20fda50704189
james.alcaraz <alcarazjames@gmail.com>
2017-02-27 15:20:21 +0800

    Created Web Api Project with Token Based Authentication

---
+++ JamesAlcaraz.NlayeredAppDemo.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JamesAlcaraz.NlayeredAppDemo.WebApi.WebApiApplication" Language="C#" %>
---
+++ JamesAlcaraz.NlayeredAppDemo.WebApi/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using JamesAlcaraz.NlayeredAppDemo.WebApi.App_Start;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+[assembly: OwinStartup(typeof(JamesAlcaraz.NlayeredAppDemo.WebApi.Startup))]
+namespace JamesAlcaraz.NlayeredAppDemo.WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureOAuth(app);
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+    }
+}
